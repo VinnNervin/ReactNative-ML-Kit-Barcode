1f2811083325f69d761aac2ab5b50a7bd28f7f78
VinnNervin <lordcharles911@gmail.com>
2025-07-31 21:49:54 +0700

    temp fixing

--- android/app/src/main/java/com/barcodekotlin/BarcodeScannerActivity.kt
+++ android/app/src/main/java/com/barcodekotlin/BarcodeScannerActivity.kt
@@ -3,7 +3,10 @@ package com.barcodekotlin
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
@@ -15,6 +18,23 @@ class BarcodeScannerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        Log.d("BarcodeScannerActivity", "Starting barcode scanner")
+
+        // Cek ketersediaan Google Play Services
+        val googleApiAvailability = GoogleApiAvailability.getInstance()
+        val resultCode = googleApiAvailability.isGooglePlayServicesAvailable(this)
+        
+        if (resultCode != ConnectionResult.SUCCESS) {
+            Log.e("BarcodeScannerActivity", "Google Play Services not available: $resultCode")
+            val resultIntent = Intent().apply {
+                putExtra("error", "SCAN_FAILED")
+                putExtra("errorMessage", "Google Play Services not available")
+            }
+            setResult(RESULT_CANCELED, resultIntent)
+            finish()
+            return
+        }
 
         // Inisialisasi dengan opsi
         val options = GmsBarcodeScannerOptions.Builder()
@@ -26,6 +46,7 @@ class BarcodeScannerActivity : AppCompatActivity() {
         // Mulai scan langsung
         scanner.startScan()
             .addOnSuccessListener { barcode: Barcode ->
+                Log.d("BarcodeScannerActivity", "Scan success: ${barcode.rawValue}")
                 val resultIntent = Intent().apply {
                     putExtra("barcode", barcode.rawValue)
                 }
@@ -33,11 +54,20 @@ class BarcodeScannerActivity : AppCompatActivity() {
                 finish()
             }
             .addOnCanceledListener {
-                setResult(RESULT_CANCELED)
+                Log.d("BarcodeScannerActivity", "Scan cancelled by user")
+                val resultIntent = Intent().apply {
+                    putExtra("error", "USER_CANCELLED")
+                }
+                setResult(RESULT_CANCELED, resultIntent)
                 finish()
             }
-            .addOnFailureListener {
-                setResult(RESULT_CANCELED)
+            .addOnFailureListener { exception ->
+                Log.e("BarcodeScannerActivity", "Scan failed", exception)
+                val resultIntent = Intent().apply {
+                    putExtra("error", "SCAN_FAILED")
+                    putExtra("errorMessage", exception.message ?: "Unknown error")
+                }
+                setResult(RESULT_CANCELED, resultIntent)
                 finish()
             }
     }
